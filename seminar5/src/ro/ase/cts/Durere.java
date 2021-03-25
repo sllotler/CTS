adcd072131af6a386a0eeb6c98995f60b6435510
calin <calin@DESKTOP-SLO5HTA>
2021-03-25 08:24:52 +0200

    Last Commit

---
+++ seminar5/src/ro/ase/cts/Durere.java
@@ -0,0 +1,22 @@
+package ro.ase.cts;
+
+public class Durere extends Categorie {
+
+	public Durere(float pretDeBaza) {
+		super(pretDeBaza);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Durere [pretDeBaza=");
+		builder.append(pretDeBaza);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+
+}
+
