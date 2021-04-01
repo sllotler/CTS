fdc6f58661feba85d07b23deb403a4f006fd3592
calin <calin@DESKTOP-SLO5HTA>
2021-04-01 08:23:54 +0300

    Last commit

---
+++ seminar 6/src/ro/ase/cts/classes/v2builder/Main/Program.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.classes.v2builder.Main;
+
+import ro.ase.cts.classes.v2builder.BuilderV2;
+import ro.ase.cts.classes.v2builder.Rezervare;
+
+public class Program {
+
+	public static void main(String[] args) {
+		BuilderV2 builder = new BuilderV2();
+		builder.setBauturaRacoritoare(true);
+		builder.setMancareInclusa(true);
+		
+		Rezervare rezervare1 = builder.setCodRezervare(5).build();
+		Rezervare rezervare2 = builder.setCodRezervare(6).build();
+		
+		System.out.println(rezervare1.toString());
+		System.out.println(rezervare2.toString());
+	}
+}
